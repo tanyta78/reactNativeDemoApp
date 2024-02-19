937f878ed69c1c240ccc799c498873b7d0b525d1
Tatyana Milanova <tatyana.milanova@mentormate.com>
2024-02-19 13:47:59 +0200

    Added camera usage image picker

---
+++ AwesomeProject/components/Places/ImagePicker.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
+
+import {
+  PermissionStatus,
+  launchCameraAsync,
+  useCameraPermissions,
+} from "expo-image-picker";
+import { COLORS } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
+
+export default function ImagePicker() {
+  const [cameraPermissionInfo, requestPermission] = useCameraPermissions();
+  const [image, setImage] = useState(null);
+
+  async function verifyPermissions() {
+    if (cameraPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to use this app."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  const takeImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    let result = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+
+    setImage(result.assets[0].uri);
+  };
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (image) {
+    imagePreview = (
+      <Image
+        source={{ uri: image }}
+        style={styles.image}
+      />
+    );
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton
+        onPress={takeImageHandler}
+        icon="camera"
+      >
+        Take Image
+      </OutlinedButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
--- AwesomeProject/components/Places/PlaceForm.js
+++ AwesomeProject/components/Places/PlaceForm.js
@@ -1,12 +1,48 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+
+import { COLORS } from "../../constants/colors";
+import ImagePicker from "./ImagePicker";
 
 export default function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
+
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
-  )
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+          style={styles.input}
+        />
+      </View>
+      <ImagePicker/>
+    </ScrollView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: COLORS.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: COLORS.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: COLORS.primary100,
+  },
+});
