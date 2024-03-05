9ac05b53776162aef8ea6a2436ecc46bb0ab9cb2
Tatyana Milanova <tatyana.milanova@mentormate.com>
2024-03-05 18:07:11 +0200

    Add video recorder

---
+++ AwesomeProject/components/Video/VideoPlayer.js
@@ -0,0 +1,123 @@
+import { Camera } from 'expo-camera'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const VideoPlayer = () => {
+  const [hasAudioPermission, setHasAudioPermission] = useState(null)
+  const [hasCameraPermission, setHasCameraPermission] = useState(null)
+  const [camera, setCamera] = useState(null)
+  const [record, setRecord] = useState(null)
+  const [type, setType] = useState(Camera.Constants.Type.back)
+  const video = React.useRef(null)
+  const [status, setStatus] = React.useState({})
+
+  useEffect(() => {
+    const checkPermissions = async () => {
+      const cameraStatus = await Camera.requestPermissionsAsync()
+      setHasCameraPermission(cameraStatus.status === 'granted')
+      const audioStatus = await Camera.requestMicrophonePermissionsAsync()
+      setHasAudioPermission(audioStatus.status === 'granted')
+    }
+    checkPermissions()
+  }, [])
+
+  const takeVideo = async () => {
+    if (camera) {
+      const data = await camera.recordAsync({
+        VideoQuality: ['2160p'],
+        maxDuration: 10,
+        maxFileSize: 200,
+        mute: false,
+        videoBitrate: 5000000
+      })
+      setRecord(data.uri)
+      console.log(data.uri)
+    }
+  }
+
+  const stopVideo = async () => {
+    camera.stopRecording()
+  }
+
+  if (hasCameraPermission === null || hasAudioPermission === null) {
+    return <View />
+  }
+  if (hasCameraPermission === false || hasAudioPermission === false) {
+    return <Text>No access to camera</Text>
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={styles.cameraContainer}>
+        <Camera
+          ref={(ref) => setCamera(ref)}
+          style={styles.fixedRatio}
+          type={type}
+          ratio={'4:3'}
+        />
+      </View>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: record
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying
+              ? video.current.pauseAsync()
+              : video.current.playAsync()
+          }
+        />
+      </View>
+      <Button
+        title="Flip Video"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          )
+        }}
+      ></Button>
+      <Button
+        title="Take video"
+        onPress={() => takeVideo()}
+      />
+      <Button
+        title="Stop Video"
+        onPress={() => stopVideo()}
+      />
+    </View>
+  )
+}
+
+export default VideoPlayer
+
+const styles = StyleSheet.create({
+  cameraContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1
+  },
+  video: {
+    alignSelf: 'center',
+    width: 350,
+    height: 220
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
