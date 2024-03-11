4d7dcfce76df9d88eba58ef28f72c8dea97da8d8
Tatyana Milanova <tatyana.milanova@mentormate.com>
2024-03-11 15:43:29 +0200

    Add github actions demo project

---
+++ GithubActionsDemoApp/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <div id="logo-img">
+          <img src={logo} />
+        </div>
+        <h1>Learn & Master GitHub Actions</h1>
+      </header>
+      <MainContent />
+    </>
+  )
+}
+
+export default App
---
+++ GithubActionsDemoApp/vite.config.js
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import eslint from 'vite-plugin-eslint'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslint()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js'
+  }
+})
